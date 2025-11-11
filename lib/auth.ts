67d1bdee871866af3bb8d4e975c75506e51a0f5b
krzysztofe <gitaragra@gmai.com>
2025-11-11 14:36:18 +0100

    Add env to auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,6 +3,9 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 
 export const auth = betterAuth({
+  secret: process.env.BETTER_AUTH_SECRET!,
+  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
+
   database: prismaAdapter(prisma, {
     provider: "sqlite",
   }),
